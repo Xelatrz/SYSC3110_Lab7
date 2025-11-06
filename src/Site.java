e0004e49c8a7603646d46a2e9a91e1288047f680
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-06 10:25:25 -0500

    extract getBaseAmount from getBillableAmount and it's duplicate, and merge them to call from both LifelineSite and ResidentialSite

--- src/Site.java
+++ src/Site.java
@@ -4,14 +4,25 @@ public class Site {
     public Site() {}
 
     public static double getBillableAmount(int _units, int _rate) {
-        double base = LifelineSite.getBaseAmount(_units, _rate);
+        double base = getBaseAmount(_units, _rate);
         double tax = LifelineSite.getTaxAmount(base);
         return base + tax;
     }
 
+    private static double getBaseAmount(int _units, int _rate, boolean lifelineTrue) {
+        if (lifelineTrue) {
+            return LifelineSite.getBaseAmount(_units, _rate);
+        }
+        return ResidentialSite.getBaseAmount(_units, _rate);
+    }
+
     public static double getBillableAmount(int _units, int _rate) {
-        double base = ResidentialSite.getBaseAmount(_units, _rate);
+        double base = getAmount(_units, _rate);
         double tax = ResidentialSite.getTaxAmount(base);
         return base + tax;
+    }
+
+    private static int getAmount(int _units, int _rate) {
+        return ResidentialSite.getBaseAmount(_units, _rate);
     }
 }
