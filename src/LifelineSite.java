d6d0213367a06dc5b9b404bd457c62186f43c1ab
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-06 09:40:26 -0500

    move getBilledAmount to Site class

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -2,12 +2,6 @@ public class LifelineSite {
 
     public LifelineSite() {}
 
-    public static double getBillableAmount(int _units, int _rate) {
-        double base = new LifelineSite().getBaseAmount(_units, _rate);
-        double tax = new LifelineSite().getTaxAmount(base);
-        return base + tax;
-    }
-
     public double getTaxAmount(double base) {
         return base * Site.TAX_RATE * 0.2;
     }
--- src/Site.java
+++ src/Site.java
@@ -2,4 +2,10 @@ public class Site {
     static public int TAX_RATE;
 
     public Site() {}
+
+    public static double getBillableAmount(int _units, int _rate) {
+        double base = new LifelineSite().getBaseAmount(_units, _rate);
+        double tax = new LifelineSite().getTaxAmount(base);
+        return base + tax;
+    }
 }
