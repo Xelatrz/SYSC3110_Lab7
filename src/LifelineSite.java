d0983333c92d80243c5b05436fbbfec27362d33b
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-06 09:39:39 -0500

    refactor getBaseAmount to public

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -3,7 +3,7 @@ public class LifelineSite {
     public LifelineSite() {}
 
     public static double getBillableAmount(int _units, int _rate) {
-        double base = getBaseAmount(_units, _rate);
+        double base = new LifelineSite().getBaseAmount(_units, _rate);
         double tax = new LifelineSite().getTaxAmount(base);
         return base + tax;
     }
@@ -12,7 +12,7 @@ public class LifelineSite {
         return base * Site.TAX_RATE * 0.2;
     }
 
-    private static double getBaseAmount(int _units, int _rate) {
+    public double getBaseAmount(int _units, int _rate) {
         return _units * _rate * 0.5;
     }
 }
