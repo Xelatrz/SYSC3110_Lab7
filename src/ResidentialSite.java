147d6c846d5eb093d4d87d20a42ce905fa05860e
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-06 09:41:31 -0500

    extract getBaseAmount method from getBillableAmount in ResidentialSite class

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -3,8 +3,12 @@ public class ResidentialSite {
     ResidentialSite() {}
 
     public double getBillableAmount(int _units, int _rate) {
-        double base = _units * _rate;
+        double base = getBaseAmount(_units, _rate);
         double tax = base * Site.TAX_RATE;
         return base + tax;
+    }
+
+    private static int getBaseAmount(int _units, int _rate) {
+        return _units * _rate;
     }
 }
