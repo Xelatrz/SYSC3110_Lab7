ee2dd56415dd80211f0560dd2595f39e1bb9688c
Taylor Brumwell <taylor.brumwell@gmail.com>
2025-11-06 09:33:10 -0500

    extract getTaxAmount method from getBillableAmount

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -4,8 +4,12 @@ public class LifelineSite {
 
     public double getBillableAmount(int _units, int _rate) {
         double base = getBaseAmount(_units, _rate);
-        double tax = base * Site.TAX_RATE * 0.2;
+        double tax = getTaxAmount(base);
         return base + tax;
+    }
+
+    private static double getTaxAmount(double base) {
+        return base * Site.TAX_RATE * 0.2;
     }
 
     private static double getBaseAmount(int _units, int _rate) {
